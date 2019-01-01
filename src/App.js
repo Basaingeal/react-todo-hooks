c9382ee7f0e6c434ccce4101ba2b1fac605daabf
Basaingeal <michaeljcurrie136@gmail.com>
2019-01-01 14:20:22 -0600

    Initial commit. Move components into their own files.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import TodoForm from './TodoForm'
+import Todo from './Todo'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function App () {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React',
+      isCompleted: false
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false
+    },
+    {
+      text: 'Build really cool todo app',
+      isCompleted: false
+    }
+  ])
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
   }
+
+  const completeTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true
+    setTodos(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+  return (
+    <div className='app'>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
